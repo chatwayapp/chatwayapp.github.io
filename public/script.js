a2cb607d1739a196a62c3586fbf25a440f8fde8b
JiningLiu <88249105+JiningLiu@users.noreply.github.com>
2022-10-30 20:52:53 -0500

    major ui change - sidebar 1 complete

--- public/script.js
+++ public/script.js
@@ -1,5 +1,26 @@
 var script = (function () {
-    var message = "Hello, world!";
-    console.log(message);
-    console.log(typeof message);
+    // Hash detection UI changes
+    var hashes = ['#', '#people', '#messages', '#groups', '#teams', '#projects'];
+    hashChange();
+    $(window).on('hashchange', function () {
+        hashChange();
+    });
+    function hashChange() {
+        $(function () {
+            var hash = location.hash == '' ? '#' : location.hash;
+            if (hashes.includes(hash)) {
+                $('.nav-link').each(function () {
+                    if (($(this).attr('href') || '') == hash) {
+                        $(this).addClass('active');
+                    }
+                    else {
+                        $(this).removeClass('active');
+                    }
+                });
+            }
+            else {
+                location.hash = '#';
+            }
+        });
+    }
 })();
--- public/script.ts
+++ public/script.ts
@@ -1,5 +1,31 @@
+
 var script = (function () {
-  let message: string = "Hello, world!";
-  console.log(message);
-  console.log(typeof message);
+
+  // Hash detection UI changes
+
+  const hashes = ['#', '#people', '#messages', '#groups', '#teams', '#projects']
+
+  hashChange();
+
+  $(window).on('hashchange', function () {
+    hashChange();
+  });
+
+  function hashChange() {
+    $(function () {
+      const hash = location.hash == '' ? '#' : location.hash
+      if (hashes.includes(hash)) {
+        $('.nav-link').each(function () {
+          if (($(this).attr('href') || '') == hash) {
+            $(this).addClass('active');
+          } else {
+            $(this).removeClass('active');
+          }
+        });
+      } else {
+        location.hash = '#'
+      }
+    });
+  }
+
 })();
