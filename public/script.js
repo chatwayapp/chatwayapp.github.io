86e078579d359d4cd7247b13342c17e186fd7821
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2022-11-03 11:02:18 -0500

    fixed cannot find signInWithRedirect

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
-import { getAuth, signInWithPopup, signOut, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { getAuth, signInWithRedirect, signOut, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
 
 var script = (async function () {
 
