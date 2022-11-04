95e3ab14e10208f07e699d62758ce411119be16d
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2022-11-04 11:08:00 -0500

    changed way to get token

--- public/script.js
+++ public/script.js
@@ -128,30 +128,35 @@ var script = (async function () {
     }
 
     async function ghSignIn(result) {
-        console.log(result)
-        const ghCredentials = GithubAuthProvider.credentialFromResult(result);
-        if (ghCredentials) {
-            const ghToken = ghCredentials.accessToken;
-            const ghUser = result.user;
-            result.user.getIdToken(true).then(function (token) {
-                console.log('Sign in with GitHub success.')
-                console.log(ghCredentials);
-                console.log(ghToken);
-                console.log(ghUser);
-                console.log(token);
-                jwtSignIn(token).then((user) => {
-                    console.log("Successfully logged in with JWT through Realm!", user);
-                });
-            }).catch(function (error) {
-                alert('Sign in failed (Firebase - Get ID Token JWT, ${error.code}): ${error.message}');
-                console.log(ghCredentials);
-                console.log(ghToken);
-                console.log(ghUser);
-                jwtSignIn(ghToken).then((user) => {
-                    console.log("Successfully logged in with JWT through Realm!", user);
-                });
-            });
-        }
+        console.log(result.accessToken)
+        jwtSignIn(result.accessToken).then((user) => {
+            console.log("Successfully logged in with JWT through Realm!", user);
+        }).catch((error) => {
+            console.error(error);
+        });
+        // const ghCredentials = GithubAuthProvider.credentialFromResult(result);
+        // if (ghCredentials) {
+        //     const ghToken = ghCredentials.accessToken;
+        //     const ghUser = result.user;
+        //     result.user.getIdToken(true).then(function (token) {
+        //         console.log('Sign in with GitHub success.')
+        //         console.log(ghCredentials);
+        //         console.log(ghToken);
+        //         console.log(ghUser);
+        //         console.log(token);
+        //         jwtSignIn(token).then((user) => {
+        //             console.log("Successfully logged in with JWT through Realm!", user);
+        //         });
+        //     }).catch(function (error) {
+        //         alert('Sign in failed (Firebase - Get ID Token JWT, ${error.code}): ${error.message}');
+        //         console.log(ghCredentials);
+        //         console.log(ghToken);
+        //         console.log(ghUser);
+        //         jwtSignIn(ghToken).then((user) => {
+        //             console.log("Successfully logged in with JWT through Realm!", user);
+        //         });
+        //     });
+        // }
     }
 
     async function jwtSignIn(jwt) {
