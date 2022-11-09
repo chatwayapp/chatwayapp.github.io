8fd4f7e8db17389344b90963b56ae5548ee217d5
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2022-11-09 11:49:35 -0600

    added shortcut testing to anypage script

--- anypage/extension/script.js
+++ anypage/extension/script.js
@@ -1,2 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-console.log('yay loaded!', initializeApp);
+
+var show = true;
+
+var bookmarkLauncherSetup = (function () {
+    window.addEventListener('keyup', function () {
+        if (event.ctrlKey && event.altKey && String.fromCharCode(event.keyCode) == 'C') {
+            console.log('shortcut pressed', show);
+        }
+    });
+}());
