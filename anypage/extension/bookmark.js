3cd13770b1bd4a4efe4cb4a3677f183509bc054c
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2022-11-09 11:39:01 -0600

    added test script for bookmark anypage

---
+++ anypage/extension/bookmark.js
@@ -0,0 +1,4 @@
+const chatwayBookmarkScript = document.createElement('script');
+chatwayBookmarkScript.setAttribute('type', 'module');
+chatwayBookmarkScript.setAttribute('src', 'https://chatwayapp.github.io/anypage/extension/script.js');
+document.body.appendChild(chatwayBookmarkScript);
---
+++ anypage/extension/script.js
@@ -0,0 +1,2 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+console.log('yay loaded!', initializeApp);
