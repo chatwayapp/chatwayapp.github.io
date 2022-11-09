49d31497d2c50269165c458e081a2626c21f4092
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2022-11-09 15:07:04 -0600

    added test ui and code changes

--- anypage/extension/bookmark.js
+++ anypage/extension/bookmark.js
@@ -1,4 +1,1 @@
-const chatwayBookmarkScript = document.createElement('script');
-chatwayBookmarkScript.setAttribute('type', 'module');
-chatwayBookmarkScript.setAttribute('src', 'https://chatwayapp.github.io/anypage/extension/script.js');
-document.body.appendChild(chatwayBookmarkScript);
+document.body.appendChild(document.createElement('script').setAttribute('type', 'module').setAttribute('src', 'https://chatwayapp.github.io/anypage/extension/script.js'));
--- anypage/extension/script.js
+++ anypage/extension/script.js
@@ -1,10 +1,27 @@
 
 var show = true;
 
+const style = document.createElement('link');
+style.setAttribute('rel', 'stylesheet');
+style.setAttribute('href', 'https://chatwayapp.github.io/anypage/root-style.css');
+document.head.appendChild(style);
+
+const anypage = document.createElement('chatway-anypage');
+const iframe = document.createElement('iframe');
+iframe.setAttribute('src', 'https://chatwayapp.github.io/anypage/chat.html');
+anypage.appendChild(iframe);
+document.body.appendChild(anypage);
+
 var bookmarkLauncherSetup = (function () {
     window.addEventListener('keyup', function () {
         if (event.ctrlKey && event.altKey && String.fromCharCode(event.keyCode) == 'C') {
+            show = !show;
             console.log('shortcut pressed', show);
+            if (show) {
+                document.querySelector('chatway-anypage').style.display = 'block';
+            } else {
+                document.querySelector('chatway-anypage').style.display = 'none';
+            }
         }
     });
 }());
